5549f10b5246ca82f10963c596448bc0158c3107
Juanjo García <juanjo@fidenet.net>
2024-02-07 20:07:08 +0100

    Get profile games

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,28 +1,75 @@
 import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { useContext } from 'react'
 import { UserContext } from '../../App'
-import {firebaseApp} from '../../firebase/FirebaseApp'
-import { getAuth, signOut } from "firebase/auth"
-const auth = getAuth(firebaseApp)
+import {firebaseApp, db} from '../../firebase/FirebaseApp'
+import { doc, updateDoc, arrayUnion, arrayRemove, getDoc } from "firebase/firestore"
+import { GameCard } from '../games/GameCard'
+import { useFetch } from '../../useFetch'
 
 export const Profile = () => {
 
     const user = useContext(UserContext)
     const [loading, setLoading] = React.useState(true)
+    const [games, setGames] = React.useState([])
+    const [error, setError] = React.useState(null)
 
     
     useEffect(() => {
         if (user) {
-            setLoading(false)
-            console.log(user)
+            const userRef = doc(db, "users", user.uid)
+            getDoc(userRef).then((doc) => {
+                if (doc != null) {
+                    let gamesAdded = addGames(doc.data().games)
+                    console.log(gamesAdded)
+                } else {
+                    console.log("No such document!")
+                }
+                setLoading(false)
+            }).catch((error) => {
+                console.log("Error:", error)
+            })
+
         }
 
-    })
+    }, [user])
+
+    const addGames = (gamesDB) => {
+        let gamesArray = []
+        gamesDB.forEach(async game => {
+            let data
+            try {
+                const response = await fetch(`https://rawg.io/api/games/${game}?key=${import.meta.env.VITE_API_KEY}`)
+                data = await response.json()
+                
+            } catch (error) {
+                setError(error.message)
+            }finally {
+                gamesArray.push(data)
+                if(gamesArray.length === gamesDB.length) {
+                    setGames(gamesArray)
+                }
+            }
+        })
+        
+    }
 
     return (
         <section className='content profile padding'>
             {loading && <div className='lds-dual-ring'></div>}
-            {user && <h2 className='page-title'>{user?.displayName ? user?.displayName : user?.email}<img src={user.photoURL ? user.photoURL : '../../../public/profile-placeholder.jpg'} className='photo'/> </h2>}
+            {user && <h2 className='page-title'><img src={user.photoURL ? user.photoURL : '../../../public/profile-placeholder.jpg'} className='photo'/> {user?.displayName ? user?.displayName : user?.email}</h2>}
+
+            <div className='library'>
+                <p className='library-title'>Library <img src='./../public/added.png'/></p>
+                
+                <article className='game-list'>
+                    {games.length > 0 ? games.map(game => (
+                        <GameCard key={game.id} data={game} />
+                    )) : <p>No games added yet</p>}
+                    {error && <p>Error: {error}</p>}
+                </article>
+
+            </div>
         </section>
     )
 }
